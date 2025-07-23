2e0d23e70c85926dacf0a98a6a62ecfd6119ff8f
neeby <artemboldirew2@gmail.com>
2025-07-23 19:06:50 +0300

    start

---
+++ src/main/java/artemboldirew/obligi/ObligiApplication.java
@@ -0,0 +1,13 @@
+package artemboldirew.obligi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/artemboldirew/obligi/ObligiApplicationTests.java
@@ -0,0 +1,13 @@
+package artemboldirew.obligi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
